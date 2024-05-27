ba65873c9494db6a9ed9071202b1546ca03da707
iAmSomething <st939823@gmail.com>
2024-05-27 13:24:50 +0900

    init

---
+++ watchProject/watchProject/watchProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchProjectApp.swift
+//  watchProject
+//
+//  Created by 김태훈 on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct watchProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
